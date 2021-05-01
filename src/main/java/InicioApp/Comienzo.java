d2efff5433675e8da0e048d140c4c492a69e8f1f
Carlos Garcia <carlos_garcia_sistemas@outlook.com>
2021-04-30 19:28:05 -0500

    Se comienza a crear menu

---
+++ src/main/java/InicioApp/Comienzo.java
@@ -0,0 +1,11 @@
+package InicioApp;
+
+import  UI.Inicio;
+
+public class Comienzo {
+    public static void main(String[] args) {
+
+        Inicio.welcomeUi();
+
+    }
+}
--- src/main/java/UI/Inicio.java
+++ src/main/java/UI/Inicio.java
@@ -5,6 +5,15 @@ public class Inicio {
 
 
 
-    public static void
+    public static void welcomeUi(){
+        String messageWelcome = "Bienvenido a System Adoption 1.0\n" +
+                "----------\n" +
+                "Menu\n"+
+                "1. Login Administrador\n"+
+                "2. Login cliente"+
+                "-----------";
+
+        System.out.println(messageWelcome);
+    }
 
 }
